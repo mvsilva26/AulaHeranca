31b472786d211496487228aba0f6446ebf66e842
Michael Silva <michael.steam@outlook.com>
2022-03-07 22:48:42 -0300

    Adicionar arquivos de projeto.

---
+++ Ex2 - ArrayList/Ex2 - ArrayList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Ex2___ArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ArrayList myAL = new ArrayList();
+
+            myAL.Add("Hello");
+            myAL.Add("World");
+            myAL.Add("!");
+
+            Console.WriteLine("myAL");
+            Console.WriteLine("     Contador:   {0}", myAL.Count);
+            Console.WriteLine("     Contador:   {0}", myAL.Capacity);
+            Console.WriteLine("     Valores:    ");
+            PrintValues(myAL);
+
+        }
+
+        public static void PrintValues(IEnumerable myList)
+        {
+            foreach (Object obj in myList)
+                Console.Write("   {0}", obj);
+            Console.WriteLine();
+        }
+
+    }
+}
